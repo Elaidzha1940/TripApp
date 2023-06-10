6b8d57d119b7798e36a4372b14c5a28f0b822817
Elaidzha1940 <eli080293@gmail.com>
2023-06-10 21:48:26 +0300

    TripApp

---
+++ TripApp/TripAppApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: TripApp
+//  File: TripAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.06.2023
+//
+//  Status:
+//
+//  */
+
+import SwiftUI
+
+@main
+struct TripAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
