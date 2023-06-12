6ce4337e95044d1dd57206bf4ccb34a73eb3ac1a
Elaidzha1940 <eli080293@gmail.com>
2023-06-12 22:28:02 +0300

    TripApp

--- TripApp/ContentView.swift
+++ TripApp/ContentView.swift
@@ -13,13 +13,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+            Text("Lets Start")
         }
-        .padding()
     }
 }
 
