fcf963d6148ef485efce2fbf1a745d6aa5eeb9f9
Elaidzha1940 <eli080293@gmail.com>
2023-06-18 15:48:52 +0300

    add a new File / Resurces

---
+++ TripApp/Extensions/Resources.swift
@@ -0,0 +1,26 @@
+//
+//  Resources.swift
+//  TripApp
+//
+//  Created by Elaidzha Shchukin on 18.06.2023.
+//
+
+import Foundation
+
+enum Resources {}
+
+extension Resources {
+    
+    enum System: String, AssetConvertable {
+        case yes
+        
+    }
+    
+    enum Icon: String, AssetConvertable {
+        case yes
+    }
+    
+    enum Categories: String, AssetConvertable {
+        case yes
+    }
+}
---
+++ TripApp/Extensions/Text.swift
@@ -0,0 +1,20 @@
+//
+//  Text.swift
+//  TripApp
+//
+//  Created by Elaidzha Shchukin on 18.06.2023.
+//
+
+import SwiftUI
+
+struct Text: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Text_Previews: PreviewProvider {
+    static var previews: some View {
+        Text()
+    }
+}
