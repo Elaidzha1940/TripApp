3fe3b841ce5975b84f897fe98757d302c8fda954
Elaidzha1940 <eli080293@gmail.com>
2023-06-18 15:37:26 +0300

    added a new Folder and File / PreviewProvider

---
+++ TripApp/Extensions/PreviewProvider.swift
@@ -0,0 +1,59 @@
+//  /*
+//
+//  Project: TripApp
+//  File: PreviewProvider.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 18.06.2023
+//
+//  Status
+//
+//  */
+
+import SwiftUI
+
+public extension PreviewProvider {
+    
+    @ViewBuilder static func devices(option: PreviewProviderOption = .minAndMax, @ViewBuilder content: () -> some View) -> some View {
+        switch option {
+        case .minAndMax:
+            self.minAndMax(content: content)
+        default :
+            self.oneDevice(option: option, content: content)
+        }
+    }
+    
+    static private func minAndMax(@ViewBuilder content: () -> some View) -> some View {
+        Group{
+            content()
+                .preferredColorScheme(.dark)
+                .previewDevice(PreviewDevice(rawValue: PreviewProviderOption.iPhone14ProMax.rawValue))
+                .previewDisplayName("\(PreviewProviderOption.iPhone14ProMax.label) - Dark")
+            content()
+                .preferredColorScheme(.light)
+                .previewDevice(PreviewDevice(rawValue: PreviewProviderOption.iPhone14ProMax.rawValue))
+                .previewDisplayName("\(PreviewProviderOption.iPhone14ProMax.label) - Light")
+            content()
+                .preferredColorScheme(.dark)
+                .previewDevice(PreviewDevice(rawValue: PreviewProviderOption.iPhoneSE1st.rawValue))
+                .previewDisplayName("\(PreviewProviderOption.iPhoneSE1st.label) - Dark")
+            content()
+                .preferredColorScheme(.light)
+                .previewDevice(PreviewDevice(rawValue: PreviewProviderOption.iPhoneSE1st.rawValue))
+                .previewDisplayName("\(PreviewProviderOption.iPhoneSE1st.label) - Light")
+        }
+    }
+    
+    static private func oneDevice(option: PreviewProviderOption, @ViewBuilder content: () -> some View) -> some View{
+        Group{
+            content()
+                .preferredColorScheme(.dark)
+                .previewDevice(PreviewDevice(rawValue: option.rawValue))
+                .previewDisplayName("\(option.label) - Dark")
+            content()
+                .preferredColorScheme(.light)
+                .previewDevice(PreviewDevice(rawValue: option.rawValue))
+                .previewDisplayName("\(option.label) - Light")
+        }
+    }
+}
+
