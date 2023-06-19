79154ea5becc01c32d31e82935e4ba6a37659007
Elaidzha1940 <eli080293@gmail.com>
2023-06-19 17:48:06 +0300

    added a New File  / UserProfile

---
+++ TripApp/UserProfile.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: TripApp
+//  File: UserProfile.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 19.06.2023
+//
+//  Status: #In progress | #Not decorated
+//
+//  */
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        UserProfile()
+    }
+}
