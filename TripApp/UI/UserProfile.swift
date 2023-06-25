e26329a7fb80e1c21b60810a1f38f05a6cbedaf4
Elaidzha1940 <eli080293@gmail.com>
2023-06-25 19:54:07 +0300

    fixing UserProfile done for now

--- TripApp/UI/UserProfile.swift
+++ TripApp/UI/UserProfile.swift
@@ -24,20 +24,22 @@ struct UserProfile: View {
         
         VStack {
             
-            Text("Добро пожаловать")
-                .font(.custom("Inter-Medium", size: 20))
-                .fontWeight(.thin)
-                .foregroundColor(Color(toText: .mainblack))
-            
-            Divider()
-                .frame(height: 1)
-                .background(Color.black)
-            
-            // ScrollView {
-            
-            Text("Давайте знакомиться")
-                .font(.custom("Inter-Regular", size: 28))
-                .foregroundColor(Color(toText: .mainblue))
+            VStack {
+                Text("Добро пожаловать")
+                    .font(.custom("Inter-Medium", size: 20))
+                    .fontWeight(.thin)
+                    .foregroundColor(Color(toText: .mainblack))
+                
+                Divider()
+                    .frame(height: 1)
+                    .background(Color.black)
+                
+                // ScrollView {
+                
+                Text("Давайте знакомиться")
+                    .font(.custom("Inter-Regular", size: 28))
+                    .foregroundColor(Color(toText: .mainblue))
+            }
             
             VStack(alignment: .leading, spacing: 10) {
                 
@@ -52,7 +54,6 @@ struct UserProfile: View {
             .foregroundColor(Color(toText: .blacktext))
             .frame(width: 290)
             .padding()
-            
             
             VStack(alignment: .leading, spacing: 10) {
                 
@@ -76,18 +77,6 @@ struct UserProfile: View {
                     .font(.custom("Inter-Regular", size: 9))
                     .foregroundColor(Color(toText: .greytext))
                 
-                
-                TextField("", text: $famNameFieldText)
-                    .textFieldStyle(.roundedBorder)
-                    .border(Color(toText: .greytext))
-                    .font(.custom("Inter-Regular", size: 15))
-                    .foregroundColor(Color(toText: .textfieldgrey))
-                
-                Text("Отчество")
-                    .font(.custom("Inter-Regular", size: 9))
-                    .foregroundColor(Color(toText: .greytext))
-                
-                
                 TextField("yourmail.com", text: $mailFieldText)
                     .textFieldStyle(.roundedBorder)
                     .border(Color(toText: .greytext))
@@ -97,7 +86,6 @@ struct UserProfile: View {
                 Text("E-mail будет логином и основным средством связи с вами")
                     .font(.custom("Inter-Regular", size: 9))
                     .foregroundColor(Color(toText: .greytext))
-                
                 
                 TextField("+7_", text: $telFieldText)
                     .textFieldStyle(.roundedBorder)
@@ -110,51 +98,51 @@ struct UserProfile: View {
                     .foregroundColor(Color(toText: .greytext))
                 
                 
-                //                    TextField("ссылка на", text: $netFieldText)
-                //                        .textFieldStyle(.roundedBorder)
-                //                        .border(Color(toText: .greytext))
-                //                        .font(.custom("Inter-Regular", size: 15))
-                //                        .foregroundColor(Color(toText: .textfieldgrey))
-                //
-                //                    //Text("Фамилия")
-                //                       // .font(.custom("Inter-Regular", size: 9))
                 
-                //                    VStack {
-                //                        Button {
-                //                            //action
-                //                        } label: {
-                //                            Text("Войти")
-                //                                .font(.custom("Inter-Medium", size: 20))
-                //                                .foregroundColor(Color(toText: .mainblack))
-                //                                .frame(width: 340, height: 50)
-                //                                .background(Color(toElement: .buttoncolor))
-                //                                .cornerRadius(60)
-                //                                .padding()
-                //                        }
-                //                    }
+                TextField("ссылка на соцсети", text: $netFieldText)
+                    .textFieldStyle(.roundedBorder)
+                    .border(Color(toText: .greytext))
+                    .font(.custom("Inter-Regular", size: 15))
+                    .foregroundColor(Color(toText: .textfieldgrey))
+                
+                Text("Фамилия")
+                    .font(.custom("Inter-Regular", size: 9))
+                    .foregroundColor(Color(toText: .textfieldgrey))
+                
+                
+                //                Button {
+                //                    //action
+                //                } label: {
+                //                    Text("Войти")
+                //                        .font(.custom("Inter-Medium", size: 20))
+                //                        .foregroundColor(Color(toText: .mainblack))
+                //                        .frame(width: 340, height: 50)
+                //                        .background(Color(toElement: .buttoncolor))
+                //                        .cornerRadius(60)
+                //                        .padding()
+                //                }
+                
             }
             .padding()
             
             Text("Используя приложение, вы даете согласие ФСТР на")
-                .font(.custom("Inter-Medium", size: 10))
+                .font(.custom("Inter-Medium", size: 12))
                 .foregroundColor(Color(toText: .mainblack))
             
             Button {
                 //action
             } label: {
-                Text("на обработку персональный данных")
-                    .font(.custom("Inter-Medium", size: 12))
+                Text("обработку персональный данных")
+                    .font(.custom("Inter-Medium", size: 14))
                     .underline()
                     .foregroundColor(Color(toText: .mainblue))
                     .multilineTextAlignment(.center)
                 
             }
-            
-            Spacer()
-    
+                        
             VStack {
+                
                 TabMenu()
-                    .padding(.top, 70)
                     .padding(.bottom, -10)
                 
                 HStack{
@@ -175,8 +163,8 @@ struct UserProfile: View {
                     }
                 }
             }
+            .padding(.bottom, -35)
         }
-       //.padding()
     }
 }
 
