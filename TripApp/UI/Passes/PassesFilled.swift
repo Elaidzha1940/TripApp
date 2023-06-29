5f86a4d670434d53f276349d8eb21aff592fd696
Elaidzha1940 <eli080293@gmail.com>
2023-06-29 21:39:55 +0300

    added a New File / PassesFilled

---
+++ TripApp/UI/Passes/PassesFilled.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: TripApp
+//  File: PassesFilled.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 29.06.2023
+//
+//  S
+//
+//  */
+
+import SwiftUI
+
+struct PassesFilled: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PassesFilled_Previews: PreviewProvider {
+    static var previews: some View {
+        PassesFilled()
+    }
+}
