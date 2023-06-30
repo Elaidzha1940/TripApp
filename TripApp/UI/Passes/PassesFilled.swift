a590a20ba005a5497ccc85525c75ee9255599044
Elaidzha1940 <eli080293@gmail.com>
2023-06-30 18:20:38 +0300

    almost done with PassesFilled

--- TripApp/UI/Passes/PassesFilled.swift
+++ TripApp/UI/Passes/PassesFilled.swift
@@ -33,18 +33,16 @@ struct PassesFilled: View {
                 VStack {
                     Group {
                         FloatingLabelTextField(placeholder: "Название перевала", text: self.$passesFilled)
-                        
                     }
                     .padding(.trailing)
                 }
                 .padding(.horizontal, 40)
-                .padding(.bottom, 20)
                 
                 HStack(spacing: 30) {
                     
                     Text("Категория перевала")
                         .font(.custom(Fonts.Inter.regular, size: 20))
-
+                        .padding()
                         Spacer()
                     
                     ZStack {
@@ -54,9 +52,181 @@ struct PassesFilled: View {
                     }
                     .frame(width: 20, height: 25)
                     .foregroundColor(Color(toElement: .mainblue))
+                    .padding()
                 }
-                .padding(5)
                 .padding(.horizontal, 30)
+                
+                //
+                HStack(spacing: 30) {
+                    ZStack {
+                        Text("H/K")
+                            .font(.custom(Fonts.Inter.regular, size: 16))
+                            .foregroundColor(
+                                Color.init(toText: .bluecom)
+                            )
+                    }
+                    .frame(width: 70, height: 45)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke()
+                    )
+                    .foregroundColor(Color(toElement: .greycom))
+                    .padding(.horizontal, 30)
+                    
+                    ZStack {
+                        Text("1А")
+                            .font(.custom(Fonts.Inter.regular, size: 16))
+                            .foregroundColor(
+                                Color.init(toText: .main)
+                            )
+                    }
+                    .frame(width: 70, height: 45)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke()
+                    )
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(Color(toElement: .mainblue))
+                    )
+                    .padding(.horizontal, 30)
+                    
+                    ZStack {
+                        Text("1Б")
+                            .font(.custom(Fonts.Inter.regular, size: 16))
+                            .foregroundColor(
+                                Color.init(toText: .bluecom)
+                            )
+                    }
+                    .frame(width: 70, height: 45)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke()
+                    )
+                    .foregroundColor(Color(toElement: .greycom))
+                    .padding(.horizontal, 30)
+                }
+                .padding()
+                
+                HStack(spacing: 37) {
+                    
+                    ZStack {
+                        Text("2А")
+                            .font(.custom(Fonts.Inter.regular, size: 16))
+                            .foregroundColor(
+                                Color.init(toText: .bluecom)
+                            )
+                    }
+                    .frame(width: 70, height: 45)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke()
+                    )
+                    .foregroundColor(Color(toElement: .greycom))
+                    
+                    ZStack {
+                        Text("2Б")
+                            .font(.custom(Fonts.Inter.regular, size: 16))
+                            .foregroundColor(
+                                Color.init(toText: .bluecom)
+                            )
+                    }
+                    .frame(width: 70, height: 45)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke()
+                    )
+                    .foregroundColor(Color(toElement: .greycom))
+                    
+                    ZStack {
+                        Text("3А")
+                            .font(.custom(Fonts.Inter.regular, size: 16))
+                            .foregroundColor(
+                                Color.init(toText: .bluecom)
+                            )
+                    }
+                    .frame(width: 70, height: 45)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke()
+                    )
+                    .foregroundColor(Color(toElement: .greycom))
+                    
+                    ZStack {
+                        Text("3Б")
+                            .font(.custom(Fonts.Inter.regular, size: 16))
+                            .foregroundColor(
+                                Color.init(toText: .bluecom)
+                            )
+                    }
+                    .frame(width: 70, height: 45)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke()
+                    )
+                    .foregroundColor(Color(toElement: .greycom))
+                }
+                .padding(.bottom, 5)
+                
+                VStack {
+                    
+                    HStack(spacing: 33) {
+                        
+                        VStack {
+                            Text("Добавить")
+                                .font(.custom(Fonts.Inter.medium, size: 14))
+                            
+                            ZStack {
+                                Image("system-starw")
+                                    .font(.custom(Fonts.Inter.regular, size: 16))
+                                    .foregroundColor(
+                                        Color.init(toText: .main)
+                                    )
+                            }
+                            .frame(width: 70, height: 45)
+                            .background(
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(Color(toElement: .mainblue))
+                            )
+                            .padding(.horizontal)
+                        }
+                        
+                        ZStack {
+                            Text("не уверен")
+                                .font(.custom(Fonts.Inter.regular, size: 16))
+                                .foregroundColor(
+                                    Color.init(toText: .main)
+                                )
+                        }
+                        .frame(width: 95, height: 45)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color(toElement: .mainblue))
+                        )
+                        .foregroundColor(Color(toElement: .greycom))
+                        .padding(.leading, 5)
+                        .padding(.horizontal)
+                        
+                        ZStack {
+                            Text("оценочно")
+                                .font(.custom(Fonts.Inter.regular, size: 16))
+                                .foregroundColor(
+                                    Color.init(toText: .bluecom)
+                                )
+                        }
+                        .frame(width: 95, height: 45)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke()
+                        )
+                        .foregroundColor(Color(toElement: .greycom))
+                        .padding(.horizontal)
+                    }
+                    .padding()
+                    .padding(.top)
+                    
+                }
+                
             }
         }
     }
